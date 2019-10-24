397e824e99d39697f46f18dcff243542d49ffdb1
Thomas Fredriksen <thomas@kaldpost.net>
2019-10-24 10:33:58 +0200

    First version

---
+++ rpi-internet-button.py
@@ -0,0 +1,37 @@
+import RPi.GPIO as GPIO
+import os
+
+internet_on = False
+
+def enable_internet():
+	global internet_on
+	if internet_on == False:
+		print("Enabling internet")
+		os.system("ifconfig eth1 up")
+		internet_on = True
+
+def disable_internet():
+	global internet_on
+	if internet_on == True:
+		print("Disabling internet")
+		os.system("ifconfig eth1 down")
+		internet_on = False
+
+def button_callback(channel):
+	if GPIO.input(channel) == GPIO.HIGH:
+		enable_internet()
+	else:
+		disable_internet()
+
+GPIO.setwarnings(False)
+GPIO.setmode(GPIO.BOARD)
+GPIO.setup(7, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+
+GPIO.add_event_detect(7, GPIO.BOTH, callback = button_callback)
+
+while True:
+	GPIO.input(7)
+	#Do nothing
+
+
+GPIO.cleanup()
